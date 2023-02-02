91316dad8d489c0f66937c863c391fe83613011f
Rigoberto Rosa <rigorosa@bu.edu>
2023-02-02 16:10:01 -0500

    Create interface for furture stack implementations

---
+++ src/com/rigoshelloworld/data/structures/Stacks/IStack.java
@@ -0,0 +1,18 @@
+package com.rigoshelloworld.data.structures.Stacks;
+public interface IStack<T> {
+/*
+ * Empty,               DONE
+ * peek,                DONE     returns but does not remove the last element in the list 
+ * pop,                 DONE     returns and removes the last element in the list                 
+ * push,                DONE
+ * search,              DONE
+ * size                 DONE
+ */
+
+    public boolean  isEmpty();
+    public T        peek();
+    public T        pop();
+    public void     push(T data);
+    public int      search(T data);
+    public int      size();    
+}
