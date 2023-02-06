8e9f938b5d76c13cad4ec08178818a4d3024ea7a
Rigoberto Rosa <rigorosa@bu.edu>
2023-02-06 12:54:16 -0500

    Correct out of bounds error on array

--- src/com/rigoshelloworld/data/structures/Stacks/ArrayBasedStack.java
+++ src/com/rigoshelloworld/data/structures/Stacks/ArrayBasedStack.java
@@ -6,14 +6,14 @@ import java.util.EmptyStackException;
 public class ArrayBasedStack<T> implements IStack<T> {
 
     private T[] array;
-    private int size = -1;
+    private int size = 0;
     private int DEFAULT_CAPACITY = 10;
 
-    ArrayBasedStack(int capacity) {
+    public ArrayBasedStack(int capacity) {
         array = (T[]) new Object[capacity];
     }
 
-    ArrayBasedStack() {
+    public ArrayBasedStack() {
         array = (T[]) new Object[DEFAULT_CAPACITY];
     }
 
@@ -22,7 +22,8 @@ public class ArrayBasedStack<T> implements IStack<T> {
         if(size >= array.length){
             grow();
         }
-        array[++size] = data;
+        array[size++] = data;
+       
     }
 
     private void grow() {
@@ -32,36 +33,34 @@ public class ArrayBasedStack<T> implements IStack<T> {
 
     @Override
     public boolean isEmpty() {
-        return size < 0 ? true : false;
+        return size == 0 ? true : false;
     }
 
     @Override
     public T peek() {
-        if(size < 0) {
+        if(size == 0) {
             throw new EmptyStackException();
         }
-        T data = array[size];
+
+        T data = array[size -1];
         return data;
     }
 
     @Override
     public T pop() {
-        if(size < 0) {
+        if(size == 0) {
             throw new EmptyStackException();
         }
-
-        T data = array[size];
-        array[size--] = null;
+        //[1,2,3]
+        T data = array[size-1];
+        array[--size] = null;
         return data;
     }
 
     @Override
     public int search(T data) {
-        if(size < 0) {
-            throw new EmptyStackException();
-        }
         
-        for(int i = size, distanceFromTop =1; i >=0 ; i--, distanceFromTop++) {
+        for(int i = size -1, distanceFromTop =1; i >= 0 ; i--, distanceFromTop++) {
             if(array[i].equals(data)){
                 return distanceFromTop;
             }
@@ -71,19 +70,28 @@ public class ArrayBasedStack<T> implements IStack<T> {
 
     @Override
     public int getSize() {
-        return size < 0 ? 0 : size;
+        return size == 0 ? 0 : size ;
     }
 
 
     
     public static void main(String[] args) {
-        ArrayBasedStack<Integer> stack = new ArrayBasedStack<>(10);
-        System.out.println(stack.size);
+        ArrayBasedStack<Integer> stack = new ArrayBasedStack<>();
+        System.out.println(stack.getSize());
         System.out.println(stack.isEmpty());
 
+
+        
         stack.push(10);
         stack.push(20);
         stack.push(30);
+
+        System.out.println(stack.search(30));
+        System.out.println(stack.search(20));
+        System.out.println(stack.search(10));
+        System.out.println();
+        System.out.println();
+
 
         System.out.println(stack.peek());
         System.out.println(stack.pop());
